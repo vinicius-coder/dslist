b0e506dfbd05d844dddcc1dd68d5133dc7960de8
Avonts <vinialves022@gmail.com>
2023-05-14 22:44:00 -0300

    Deploy ready

--- src/main/java/com/vintech/dslist/dto/GameMinDTO.java
+++ src/main/java/com/vintech/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.vintech.dslist.dto;
 
 import com.vintech.dslist.entities.Game;
+import com.vintech.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
