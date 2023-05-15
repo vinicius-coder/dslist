7f997a2de8bd0187d53e0a5f8309009416d7d692
Avonts <vinialves022@gmail.com>
2023-05-14 22:03:07 -0300

    GET lists

---
+++ src/main/java/com/vintech/dslist/controllers/GameListController.java
@@ -0,0 +1,25 @@
+package com.vintech.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vintech.dslist.dto.GameListDTO;
+import com.vintech.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+	@Autowired
+	private GameListService gameListService;
+
+	@GetMapping
+	public List<GameListDTO> findAll() {
+		return gameListService.findAll();
+	}
+
+}
---
+++ src/main/java/com/vintech/dslist/services/GameListService.java
@@ -0,0 +1,25 @@
+package com.vintech.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vintech.dslist.dto.GameListDTO;
+import com.vintech.dslist.entities.GameList;
+import com.vintech.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+
+}
